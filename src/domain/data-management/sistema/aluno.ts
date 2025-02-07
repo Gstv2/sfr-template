cb7a85b829df51dc082e545604ceb4b2cec9c686
Nathalia557 <naty.mviana7@gmail.com>
2025-02-06 23:23:49 -0300

    Atualização  classe atendrmento
    Nova atualização

---
+++ src/domain/data-management/sistema/aluno.ts
@@ -0,0 +1,7 @@
+export class Aluno{
+    matricula: string;
+
+    constructor(matricula: string){
+        this.matricula = matricula;
+    }
+}
--- src/domain/data-management/sistema/atendimento.ts
+++ src/domain/data-management/sistema/atendimento.ts
@@ -1,4 +1,24 @@
-class Atendimento{
+import { Aluno } from "./aluno";
 
+export class Atendimento{
+    aluno: Aluno;
+    ocupado: boolean;
+
+    constructor(aluno: Aluno, ocupado: boolean){
+        this.aluno = aluno;
+        this.ocupado = ocupado;
+    }
+
+    public adicionarAluno(){
+
+    }
+
+    public removerAluno(){
+
+    }
+
+    public verificarAtendimento(){
+        
+    }
 
 }
