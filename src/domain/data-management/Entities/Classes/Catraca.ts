b3d8a4af2a6fbd5bae87cccf24375ad0d673a34b
Arthur Santana <arthursantana152020@gmail.com>
2025-02-13 17:43:42 -0300

    Atualização da classe Catraca e design em PlantUML
    Atualização da classe Catraca e design em PlantUML: Adicionados métodos para travar e liberar a catraca com base no estado da fila interna. Alguns ajustes ainda são necessários na classe Catraca.

--- src/domain/data-management/Entities/Classes/Catraca.ts
+++ src/domain/data-management/Entities/Classes/Catraca.ts
@@ -3,37 +3,58 @@ import { Aluno } from './Aluno.ts'; // Import da classe Aluno
 export class Catraca {
     private id_catraca: number;
     private aluno: Aluno | undefined;
+    private ocupada: boolean;
+    private travada: boolean; 
 
     constructor(id: number) {
         this.id_catraca = id;
         this.aluno = undefined;
+        this.ocupada = false;
+        this.travada = false; 
     }
 
     public AddAluno(aluno: Aluno): void {
-        if (!aluno) {
-            throw new Error("Aluno inválido.");
+        if (this.travada) {
+            throw new Error("Catraca travada! Aguarde liberação.");
         }
 
-        if (this.aluno === undefined) {
-            this.aluno = aluno;
-            console.log(`Aluno com ID ${aluno.id} entrou na catraca.`);
-        } else {
-            console.log("Catraca ocupada! Aguarde a liberação.");
+        if (this.ocupada) {
+            throw new Error("Catraca ocupada! Aguarde a liberação antes de adicionar um novo aluno.");
         }
+
+        this.aluno = aluno;
+        this.ocupada = true;
+        console.log(`Aluno com ID ${aluno.id} entrou na catraca.`);
     }
 
     public DelAluno(): Aluno | undefined {
-        if (!this.aluno) {
+        if (!this.ocupada) {
             throw new Error("Nenhum aluno presente na catraca.");
         }
 
-        const alunoRemovido = this.aluno;
+        let alunoRemovido = this.aluno;
         this.aluno = undefined;
-        console.log(`Aluno com ID ${alunoRemovido.id} saiu da catraca.`);
+        this.ocupada = false;
+
+        console.log(`Aluno com ID ${alunoRemovido?.id} saiu da catraca.`);
         return alunoRemovido;
     }
 
     public verificarOcupado(): boolean {
-        return this.aluno !== undefined;
+        return this.ocupada;
+    }
+
+    public TravarCatraca(): void {
+        this.travada = true;
+        console.log("Catraca travada! Fila interna cheia.");
+    }
+
+    public LiberarCatraca(): void {
+        this.travada = false;
+        console.log("Catraca liberada! Há espaço na fila interna.");
+    }
+
+    public verificarTravada(): boolean {
+        return this.travada;
     }
 }
