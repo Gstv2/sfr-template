e80e3a1cc8dc6296d1b7caa9bfd0425de5aebedc
Arthur Santana <arthursantana152020@gmail.com>
2025-02-24 20:45:17 -0300

    Update ChegadaCatraca.ts
    atualização

--- src/domain/data-management/Entities/eventos/ChegadaCatraca.ts
+++ src/domain/data-management/Entities/eventos/ChegadaCatraca.ts
@@ -4,13 +4,13 @@ import { Catraca } from "../sistema/Catraca";
 import { MaquinaEventos } from "./MaquinaDeEventos";
 
 export class ChegadaCatraca extends Evento {
-    constructor(
-        private aluno: Aluno,
-        timestamp: number,
-        private catraca: Catraca,
-        maquina: MaquinaEventos
-    ) {
+    aluno: Aluno; 
+    catraca: Catraca;
+
+    constructor(aluno: Aluno, timestamp: number, catraca: Catraca, maquina: MaquinaEventos) {
         super(timestamp, catraca, maquina);
+        this.aluno = aluno; 
+        this.catraca = catraca;
     }
 
     processarEvento(): void {
